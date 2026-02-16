2665f324414cfacc68fc97947985cd547b398693
Ali Hussein <ah0979075074@gmail.com>
2026-02-16 23:03:40 +0300

    define urls

---
+++ echo/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.views.generic import RedirectView
+
+urlpatterns= [
+    path('home/', views.home, name="home"),
+    path('', RedirectView.as_view(url='/home/', permanent=True)),
+    path('auth/', views.auth, name='auth'),
+    path('chat/', views.chat, name="chat")
+]
